4a4f9ffeab20820549a1ca12a333765697379033
Jonathan Edwards <88948334+euplayer2@users.noreply.github.com>
2024-12-14 19:12:53 -0300

    first commit

---
+++ src/main/java/dev/jon/CadastroDeClientes/Clientes/Controller/ClienteController.java
@@ -0,0 +1,16 @@
+package dev.jon.CadastroDeClientes.Clientes.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ClienteController {
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Teste mensagem na rota";
+    }
+
+
+}
