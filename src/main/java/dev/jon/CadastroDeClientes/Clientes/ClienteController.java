5ac6b73e4c46a334fc0a779ceb1579e75ddc75aa
Jonathan Edwards <88948334+euplayer2@users.noreply.github.com>
2025-01-27 16:10:41 -0300

    feat:Atualiza o controller com um mock de endpoints

--- src/main/java/dev/jon/CadastroDeClientes/Clientes/ClienteController.java
+++ src/main/java/dev/jon/CadastroDeClientes/Clientes/ClienteController.java
@@ -1,8 +1,6 @@
 package dev.jon.CadastroDeClientes.Clientes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,35 @@ public class ClienteController {
         return "Teste mensagem na rota";
     }
 
+
+    //adicionar cliente
+    @PostMapping("/criar")
+    public String criarCliente() {
+        return "cliente criado";
+    }
+
+    //procurar cliente por id
+    @GetMapping("/todosID")
+    public String mostrarTodosOsClientesPorId() {
+        return "mostrando por id";
+    }
+
+    //mostrar todos os clientes
+    @GetMapping("/all")
+    public String mostrarTodosOsClientes() {
+        return "mostrando todos";
+    }
+
+    //alterar dados clientes
+    @PutMapping("/alterarID")
+    public String mostrarClientesPorId() {
+        return "alterar";
+    }
+
+    //deletar clientes
+        @DeleteMapping("/deletarID")
+        public String deletarClientePorId() {
+            return "deletar";
+        }
 
 }
